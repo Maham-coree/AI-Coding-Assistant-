f5a3f4751e8012c9fbac48f52d00825ae3aa3a09
Mahamcoree <mahamwasim0126@gmail.com>
2026-07-09 03:46:02 -0700

    changes in app.py

--- app.py
+++ app.py
@@ -39,5 +39,8 @@ def chat():
         "index.html",
         ai_response=ai_response,
          user_input=user_input) 
-if __name__=="__main__":
-    app.run(debug=True) 
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
